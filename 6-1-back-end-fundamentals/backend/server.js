e7960f13838f5b638010349f6e2d83b568618125
SaifKFUPM <S202257480@kfupm.edu.sa>
2026-03-30 09:53:12 +0300

    TODO 5 Done

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -1,20 +1,21 @@
 // TODO 1: Import Express
 import express from 'express';
 
+// TODO 3: Allow React to access the server
+import cors from 'cors';
+
 
 
 // TODO 2: Create the Express app and store it in a variable named app
 const app = express();
-
-
-
-// TODO 3: Allow React to access the server
-import cors from 'cors';
 app.use(cors());
 
 
 
 // TODO 5: Create the home route "/"
+app.get('/', (req, res) => {
+  res.send('Hello from the back-end server');
+});
 
 
 
